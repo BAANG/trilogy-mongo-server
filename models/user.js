3180e2c4bdb5da8413c7c66e18032ecf607a8f71
BAANG <chcelestino@gmail.com>
2019-09-26 13:47:22 -0700

    install axios

--- models/user.js
+++ models/user.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = Schema ({
-  user: { type: String, required: true },
-  os: { type: String },
-  timeLog: { type: String }
-})
+const userSchema = Schema(
+  {
+    user: { type: String, required: true },
+    os: { type: String },
+    timeLog: { type: String }
+  }
+)
 
 const User = mongoose.model("User", userSchema)
 
